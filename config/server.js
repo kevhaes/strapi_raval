93cd5762872134fcad7c1f39895457aff3e99ced
Kevin Haesendonck <Kevin.Haesendonck@dematic.com>
2023-06-19 12:03:52 +0200

    fist commit

---
+++ config/server.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: 8085,
+/*  port: env.int('PORT', 8085),*/
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});

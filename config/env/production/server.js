a9d0b1dda42ba592b6b8927b4e111d95e724de10
Kevin Haesendonck <Kevin.Haesendonck@dematic.com>
2023-06-20 10:16:23 +0200

    adding gh pages config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,14 +1,4 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  host: "0.0.0.0",
-  port: process.env.PORT,
-  url: env('MY_HEROKU_URL'),
-  app: {
-    keys: env.array('APP_KEYS')
-  },
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-  },
-})
+  host: env('APP_HOST', 'kevhaes.github.io/strapi_raval'),
+  port: env.int('NODE_PORT', 1337),
+});

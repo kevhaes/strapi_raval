60526248826f6f531321331896100397074fd4ae
Kevhaes <60432749+kevhaes@users.noreply.github.com>
2023-06-30 16:17:43 +0200

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('APP_HOST', 'https://strapi-raval.onrender.com/'),
+  host: env('APP_HOST', 'strapi-raval.onrender.com'),
   port: env.int('NODE_PORT', 1337),
 });
